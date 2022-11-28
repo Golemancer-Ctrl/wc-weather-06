e64153c6aba98278b6e322fa2da37e9365764a0f
Sheev <ericmarcypro@gmail.com>
2022-11-28 16:32:25 -0500

    progressed in the submission of data to APIs, list

--- README.md
+++ README.md
@@ -15,3 +15,6 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+## Description
+In this assignment I worked to create an application for forecasting weather in an area based upon user input.  The user submits the name of a town or city which is then fed into an API to acquire a relevant latitude and longitude.  The latitude and longitude are then fed into another API designed to gather weather data regarding the area.  While incomplete, my work on this assignment instructed me in the complexity of assigning HTML elements adaptively through the use of JavaScript.
--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,34 @@
 const searchButton = document.getElementById('search-btn');
 const searchHistory = document.getElementById('search-history');
 let city = document.getElementById('city').value;
+let cityList = [localStorage.getItem('cityName')];
+let lat = null;
+let lon = null;
 
 // currently set to Long Valley Latitutde/Longitude, sub in variables for responsive functionality
-let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=40.78&lon=-74.78&appid=0d255d14ef9f7866b4a1960fdfcc2a1c";
+let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=0d255d14ef9f7866b4a1960fdfcc2a1c";
 // currently set to London, sub in text input from search feature
-let geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=0d255d14ef9f7866b4a1960fdfcc2a1c";
+let geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=0d255d14ef9f7866b4a1960fdfcc2a1c";
 
+// restructure
 searchButton.addEventListener('click', () => {
-    console.log(city.value);
-    localStorage.setItem('coordinates', city.value);
-    searchHistory.append(`<li> ${city.value} </li>`);
+    //handles the acquisition of the coordinates from geoAPI
+    console.log(geoAPI);
+    localStorage.setItem('cityName', city.value);
+    localStorage.setItem('lat', geoAPI.city.coord.lat);
+    localStorage.setItem('lon', geoAPI.city.coord.lon);
+
+    //appends the recent search to the list of prior searches
+    var newListItem = document.createElement("li");
+    //create new text node
+    var listValue = document.createTextNode(i);
+    //add text node to li element
+    newListItem.appendChild(listValue);
+    //add new list element built in previous steps to unordered list
+    searchHistory.appendChild(newListItem);
 })
+
+
 
 fetch(weatherAPI, {
     // The browser fetches the resource from the remote server without first looking in the cache.
