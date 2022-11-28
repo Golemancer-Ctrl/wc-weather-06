9fd8eb982103bb672cd7aa17cec62ee815e39ea3
Sheev <ericmarcypro@gmail.com>
2022-11-28 13:04:11 -0500

    added event listener to search button

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,17 @@
 const searchButton = document.getElementById('search-btn');
+const searchHistory = document.getElementById('search-history');
 let city = document.getElementById('city').value;
 
 // currently set to Long Valley Latitutde/Longitude, sub in variables for responsive functionality
 let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=40.78&lon=-74.78&appid=0d255d14ef9f7866b4a1960fdfcc2a1c";
 // currently set to London, sub in text input from search feature
 let geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=0d255d14ef9f7866b4a1960fdfcc2a1c";
+
+searchButton.addEventListener('click', () => {
+    console.log(city.value);
+    localStorage.setItem('coordinates', city.value);
+    searchHistory.append(`<li> ${city.value} </li>`);
+})
 
 fetch(weatherAPI, {
     // The browser fetches the resource from the remote server without first looking in the cache.
